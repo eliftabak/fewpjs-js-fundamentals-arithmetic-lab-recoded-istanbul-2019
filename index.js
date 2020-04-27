7d8c1d554d188ed14baee391e4e845303460ca4f
eliftabak <elifchorghay@gmail.com>
2020-04-27 03:16:19 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() *1) + 1;
+let mod = 19 % 5;
+let max = Math.max(1, 2, 4, 20);
